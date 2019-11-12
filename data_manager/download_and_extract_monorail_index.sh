50aa302e1363507f5c2789c1782ae8c23a6fa948
ChristopherWilks <broadsword@gmail.com>
2019-11-11 19:43:10 -0500

    added conditional for ref

--- data_manager/download_and_extract_monorail_index.sh
+++ data_manager/download_and_extract_monorail_index.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 target_dir=$1
-ref='hg38'
+#e.g. "hg38" or "ath10"
+ref=$2
+
+if [[ -z $ref ]]; then
+   ref='ath10'
+fi 
 
 mkdir -p ${target_dir}/${ref}
 for f in gtf.tar.gz unmapped_hisat2_idx.tar.gz salmon_index.tar.gz star_idx.tar.gz ; do
